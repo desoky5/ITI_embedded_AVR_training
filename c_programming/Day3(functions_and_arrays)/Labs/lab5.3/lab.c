005b4b7e0fe3f784ee0261a09df608adaf46bd3e
Omar Desoky <omardsoky05@gmail.com>
2026-07-18 08:59:38 +0300

    Lab Added

--- c_programming/Day3(functions_and_arrays)/Labs/lab5.3/lab.c
+++ c_programming/Day3(functions_and_arrays)/Labs/lab5.3/lab.c
@@ -1,29 +1,30 @@
 #include<stdio.h>
-void swap (int , int );
-
+void swap(int*,int*);
 int main()
 {
-    int arr[10],sum=0;
-    for(int i = 0 ;i<10;i++)
+    int arr[5]={4,3,5,2,1} ,sum=0 ;
+    
+    for(int i = 0 ;i<4;i++)
     {
-        printf("Please enter number %d:",i+1);
-        scanf("%d",&arr[i]);
-    }
-    for(int i = 0 ;i<10;i++)
-    {
-        if (arr[i]>arr[i+1])
+        for(int j = 0 ;j<4-i;j++)
+        if (arr[j]>arr[j+1])
         {
-            swap(arr[i],arr[i+1]);
+           swap(&arr[j],&arr[j+1]);
         }
         
     }
-    
+    for(int i = 0 ;i<5;i++)
+    {
+        printf(" %d\n",arr[i]);
+    }
+ 
 
 }
-void swap (int num1 , int num2)
-{
-    int temp , 
-    temp = num1;
-    num1=num2;
-    num2=temp;
+void swap (int* num1,int* num2)
+ {
+    int temp;
+    int * ptr = &temp; 
+    *ptr = *num2;
+    *num2=*num1;
+    *num1=*ptr;
 }

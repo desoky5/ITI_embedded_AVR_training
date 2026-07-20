a76a1a9ed901bcee5d7e5eb05d4f5bf288f4bf40
Omar Desoky <omardsoky05@gmail.com>
2026-07-20 13:47:08 +0300

    lab modified

--- c_programming/Day6(user_defined_data_types)/Labs/lab6.6/lab.c
+++ c_programming/Day6(user_defined_data_types)/Labs/lab6.6/lab.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 typedef struct 
 {
-    char hour ;
-    char minute;
-    char second;
+    int hour ;
+    int minute;
+    int second;
 }time;
 
 void diff(time time1,time time2,time * diff_struct)
@@ -19,18 +19,10 @@ int main()
     time time1;
     time time2;
     time diff_struct;
-    printf("Enter hours of time1:");
-    scanf("%hhd",&time1.hour);
-    printf("Enter minutes of time1:");
-    scanf("%hhd",&time1.minute);
-    printf("Enter seconds of time1:");
-    scanf("%hhd",&time1.second);
-    printf("Enter hours of time2:");
-    scanf("%hhd",&time2.hour);
-    printf("Enter minutes of time2:");
-    scanf("%hhd",&time2.minute);
-    printf("Enter seconds of time2:");
-    scanf("%hhd",&time2.second);
+    printf("Enter start time:");
+    scanf("%d %d %d",&time1.hour,&time1.minute,&time1.second);
+    printf("Enter end time :");
+    scanf("%d %d %d",&time2.hour,&time2.minute,&time2.second);
     diff(time1,time2,&diff_struct);
     printf("TIME DIFFERENCE : %d:%d:%d - %d:%d:%d = %d:%d:%d\n",time1.hour,time1.minute,time1.second,time2.hour,time2.minute,time2.second,diff_struct.hour,diff_struct.minute,diff_struct.second);
 

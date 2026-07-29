a35615c6f9dfacdb565de9449d8fa37887c632ea
Omar Desoky <omardsoky05@gmail.com>
2026-07-29 23:42:42 +0300

    (HAL driver) : driver for KeyPad Interfacing added

---
+++ driver/HAL/keypad/keypad_int.h
@@ -0,0 +1,16 @@
+#ifndef HAL_KPD_KPD_INT_H_
+#define HAL_KPD_KPD_INT_H_
+
+#define KPD_ROWS       3
+#define KPD_COLS       3
+
+#define NO_KEY         0xFF
+
+void HKPD_vInit(void);
+
+
+u8 HKPD_u8GetPressedKey(const u8 KeyMap[KPD_ROWS][KPD_COLS]);
+
+
+
+#endif /* HAL_KPD_KPD_INT_H_ */

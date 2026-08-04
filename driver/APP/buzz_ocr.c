4db5779764e42c4ec70c76a222ca51693d553fff
Omar Desoky <omardsoky05@gmail.com>
2026-08-04 18:57:55 +0300

    toggling the buzzer using timer0 CTC mode and callback function ISR

--- driver/APP/buzz_ocr.c
+++ driver/APP/buzz_ocr.c
@@ -1,39 +1,38 @@
-/*
- * main.c
- *
- *  Created on: Jul 29, 2026
- *      Author: Omar Desoky
- */
-#define F_CPU 8000000UL
-
-#include "../LIB/STD_TYPES.h"
-#include "../MCAL/TIMR/TIMR_int.h"
-#include "../MCAL/DIO/DIO_int.h"
-#include "../MCAL/GIE/GIE_int.h"
-
-
-void toggleLED(void)
-{
-	MDIO_vTogPinVal(DIO_PORTA, DIO_PIN0);
-}
-int main()
-{
-	MDIO_vInit();
-
-	MDIO_vSetPinDir(DIO_PORTA,DIO_PIN0,DIO_OUTPUT);
-
-	MGIE_vEnableGlobalInterrupt();
-
-	MTIMERS_vInit();
-	MTIMERS_vSetPreloadValue(TIM_0,192);
-
-	MTIMERS_vSetInterval_OVF(toggleLED, 3907);
-
-	MTIMERS_vStartTimer(TIM_0);
-
-	while(1)
-	{
-
-	}
-	return 0 ;
-}
+///*
+// * main.c
+// *
+// *  Created on: Jul 29, 2026
+// *      Author: Omar Desoky
+// */
+//#define F_CPU 8000000UL
+//
+//#include "../LIB/STD_TYPES.h"
+//#include "../MCAL/TIMR/TIMR_int.h"
+//#include "../MCAL/DIO/DIO_int.h"
+//#include "../MCAL/GIE/GIE_int.h"
+//#include "../MCAL/ADC/ADC_int.h"
+//
+//
+//void toggbuzzer(void)
+//{
+//	MDIO_vTogPinVal(DIO_PORTB,DIO_PIN1);
+//}
+//int main()
+//{
+//	u16 ADC ,OCR;
+//	MDIO_vInit();
+//	MADC_vInit();
+//	MTIMERS_vInit();
+//	MDIO_vSetPinDir(DIO_PORTB,DIO_PIN1,DIO_OUTPUT);
+//	MGIE_vEnableGlobalInterrupt();
+//	MTIMERS_vSetCompareMatch(TIM_0,100);
+//	MTIMERS_vSetInterval_CTC(toggbuzzer,1);
+//	MTIMERS_vStartTimer(TIM_0);
+//	while(1)
+//	{
+//		ADC = MADC_vReadAnalog(CHANNEL_0);
+//		OCR = 20 + ((ADC*230)/10);
+//		MTIMERS_vSetCompareMatch(TIM_0,OCR);
+//	}
+//	return 0 ;
+//}
